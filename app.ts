d3f3075ce208747479750b2eb4f9d400d6c74218
V1kt0rT2K <130016386+V1kt0rT2K@users.noreply.github.com>
2025-06-09 09:24:08 -0600

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,46 @@
+import express, { Express, Request, Response, NextFunction } from "express";
+import indexRoutes from './indexRoutes';
+import { queryResponse } from "./queryResponse";
+// import createWebSocketServer from './services/websocketService'; // Importa el servidor WebSocket
+
+const app: Express = express();
+const corsOptions = {
+    origin: '*',
+    allowedHeaders: ['Content-Type', 'Authorization', 'Auth-Token'],
+    exposedHeaders: ['Auth-Token'],
+};
+app.use(express.json());
+//app.use(cors(corsOptions));
+
+
+app.use((req:Request, res:Response, next:NextFunction) => {
+
+    res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+  
+    res.setHeader(
+      'Content-Security-Policy',
+      "default-src 'self'; script-src 'self'; object-src 'none'; frame-ancestors 'self'; style-src 'self'" 
+    );
+  
+    next();
+  });
+
+  app.use((err: { status: number; }, req:Request, res:Response, next:NextFunction) => {
+    if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+      return res.status(400).send(queryResponse.error(400, "Error en la solicitud", 3));
+    }
+    next();
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.sendStatus(401);
+});
+
+app.use(indexRoutes);
+
+//dotenv.config();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () =>{
+    console.log(`[SERVER] running at http://localhost:${port}`);
+});
---
+++ requestParams.ts
@@ -0,0 +1,22 @@
+import { Request } from 'express';
+
+export class requestParams {
+static fromRequest(input: Request | { [key: string]: any }): { json: string, user: string } {
+    let params: { [key: string]: any } = {};
+
+    if ('body' in input && 'params' in input) {
+        params = {
+            ...input.body,
+            ...input.params
+        };
+    } else {
+        
+        params = { ...input };
+    }
+
+    return {
+        json: JSON.stringify(params),
+        user: ""
+    };
+}
+}
