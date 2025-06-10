f1f827448d14711b90d9929106d7171ec9c34d63
Viktor Hernandez <victorandrehernandezvelasquez@gmail.com>
2025-06-09 18:37:34 -0600

    change requestParams

--- src/utils/requestParams.ts
+++ src/utils/requestParams.ts
@@ -1,7 +1,6 @@
 import { Request } from 'express';
-
 export class requestParams {
-static fromRequest(input: Request | { [key: string]: any }): { json: string, user: string } {
+static fromRequest(input: Request | { [key: string]: any }): { json: string} {
     let params: { [key: string]: any } = {};
 
     if ('body' in input && 'params' in input) {
@@ -15,8 +14,29 @@ static fromRequest(input: Request | { [key: string]: any }): { json: string, use
     }
 
     return {
-        json: JSON.stringify(params),
-        user: ""
+        json: JSON.stringify(params)
     };
 }
 }
+
+// export class requestParams {
+// static fromRequest(input: Request | { [key: string]: any }): { json: string, user: string } {
+//     let params: { [key: string]: any } = {};
+
+//     if ('body' in input && 'params' in input) {
+//         params = {
+//             ...input.body,
+//             ...input.params
+//         };
+//     } else {
+        
+//         params = { ...input };
+//     }
+
+//     return {
+//         json: JSON.stringify(params),
+//         user: ""
+//     };
+// }
+// }
+
