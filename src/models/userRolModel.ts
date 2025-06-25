7b66285857f5652336f2a2183284f8ba90582e40
IdaIvZe <idalia.zelaya@unah.hn>
2025-06-25 17:21:01 -0600

    Creating UserRole model for table UserRoles

---
+++ src/models/userRolModel.ts
@@ -0,0 +1,35 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../utils/connection";
+import { MAX } from 'mssql';
+
+class UserRol extends Model {}
+
+UserRol.init(
+  {
+    idRole: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    roleName: {
+      type: DataTypes.STRING('MAX'),
+      allowNull: false,
+    },
+    roleDescription: {
+      type: DataTypes.STRING('MAX'),
+      allowNull: false,
+    },
+  },
+  {
+    // Other model options go here
+    sequelize, 
+    timestamps: false,
+    modelName: 'UserRol', 
+    tableName: 'tblUserRoles',
+    schema : 'users'
+  },
+);
+
+UserRol.sync();
+
+export default UserRol;
