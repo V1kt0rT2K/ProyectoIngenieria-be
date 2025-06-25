0b1a0ab57e492818eecc1fd0878b150d66b5b445
cronoscar <eucedao662@gmail.com>
2025-06-25 10:44:04 -0600

    Creating the People table model

---
+++ src/models/personModel.ts
@@ -0,0 +1,43 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../utils/connection";
+class Person extends Model {}
+
+Person.init(
+    {
+    idPerson:{
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    identityNumber:{
+        type: DataTypes.STRING(13),
+        allowNull: false
+    },
+    firstName:{
+        type: DataTypes.STRING('MAX'),
+        allowNull: false
+    },
+    secondName :{
+        type: DataTypes.STRING('MAX'),
+        allowNull: true
+    },
+    lastName: {
+        type: DataTypes.STRING('MAX'),
+        allowNull: false
+    },
+    secondLastName:{
+        type: DataTypes.STRING('MAX'),
+        allowNull: true
+    }
+},
+{
+    sequelize,
+    timestamps: false,
+    modelName: 'Person',
+    tableName: 'tblPersons',
+    schema: 'users'
+}
+
+);
+Person.sync();  
+export default Person;
