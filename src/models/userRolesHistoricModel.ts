8e8c2b2316988fa8e1ebca92b560a47fc9e9b12f
cronoscar <eucedao662@gmail.com>
2025-06-25 18:18:36 -0600

    Making of the User Roles Historic model

---
+++ src/models/userRolesHistoricModel.ts
@@ -0,0 +1,46 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../utils/connection";
+class userRolesHistoric extends Model {}
+
+userRolesHistoric.init(
+    {
+    idUserHistoric:{
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    idUser:{
+        type: DataTypes.INTEGER,
+        allowNull:  false,
+        references: {
+            model: 'tblUsers',
+            key: 'idUser'
+        }
+    },
+    idRole:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'tblUserRoles',
+            key: 'idRole'
+        }
+    },
+    generationDate:{
+        type: DataTypes.DATE,
+        allowNull: true
+    },
+    description: {
+        type: DataTypes.STRING('MAX'),
+        
+    }
+},
+{
+    sequelize,
+    timestamps: false,
+    modelName: 'userRolesHistoric',
+    tableName: 'tbluserRolesHistorics',
+    schema: 'users'
+}
+);
+userRolesHistoric.sync();  
+export default userRolesHistoric;
