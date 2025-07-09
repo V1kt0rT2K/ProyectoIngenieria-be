ab13032a7da6d6dfe591ce662c289f4a8417c02c
IdaIvZe <idalia.zelaya@unah.hn>
2025-07-09 15:14:24 -0600

    Add stageType model

---
+++ src/models/stageTypeModel.ts
@@ -0,0 +1,31 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../utils/connection';
+
+class StageType extends Model{};
+
+StageType.init(
+    {
+        idStageType:{
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        stageTypeName:{
+            type: DataTypes.STRING('MAX'),
+            allowNull: false
+        },
+        description:{
+            type: DataTypes.STRING('MAX'),
+        }
+    },
+    {
+    sequelize, 
+    timestamps: false,
+    modelName: 'StageType', 
+    tableName: 'tblStageTypes',
+    schema : 'asset'
+  },
+)
+
+StageType.sync();
+export default StageType;
