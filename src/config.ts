c935ffc75ad4fd8b41c55037101f8a1756c435f6
V1kt0rT2K <130016386+V1kt0rT2K@users.noreply.github.com>
2025-07-24 18:36:42 -0600

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,12 @@
 //CONFIG LOCAL
 
-export const config = {
-    DB_USER : 'UserProyectoIngenieria',
-    DB_PASSWORD : 'LOSFABULOSOSCADILLAC11',
-    //DB_SERVER : 'ProyectoIngenieria.mssql.somee.com',
-    DB_SERVER: 'localhost',
-    DB_NAME : 'ProyectoIngenieria'
-};
+//export const config = {
+//   DB_USER : 'UserProyectoIngenieria',
+//    DB_PASSWORD : 'LOSFABULOSOSCADILLAC11',
+//    //DB_SERVER : 'ProyectoIngenieria.mssql.somee.com',
+//    DB_SERVER: 'localhost',
+//    DB_NAME : 'ProyectoIngenieria'
+//};
 
 export const SECRET_KEY = 'viktorhernandez';
 export const TIME_OUT = "2000s";
@@ -14,9 +14,9 @@ export const TIME_OUT = "2000s";
 
 
 //CONFIG REMOTE
-// export const config = {
-//     DB_USER : 'V1KT0RT2K_SQLLogin_1',
-//     DB_PASSWORD : 'w9robv81mj',
-//     DB_SERVER : 'ProyectoIngenieria.mssql.somee.com',
-//     DB_NAME : 'ProyectoIngenieria'
-// };
+export const config = {
+     DB_USER : 'V1KT0RT2K_SQLLogin_1',
+     DB_PASSWORD : 'w9robv81mj',
+     DB_SERVER : 'ProyectoIngenieria.mssql.somee.com',
+     DB_NAME : 'ProyectoIngenieria'
+ };
